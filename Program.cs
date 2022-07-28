80cd1a7d60edf0e30abad666d814ba2e87b70cfb
Vladimir <VL97@mail.ru>
2022-07-28 20:42:19 +0300

    программа готова

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] primordial_massive = { "hello", "2", "world", ":-)" };
+string[] final_massive = new string[primordial_massive.Length];
+int count = 0;
+int number = 3; //меньше трех
+for (int i = 0; i < primordial_massive.Length; i++)
+    if (primordial_massive[i].Length <= number)
+    {
+        final_massive[count] = primordial_massive[i];
+        count++;
+    }
